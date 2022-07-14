088b6c8973e88f5b8ceff3202a47204312c54674
diegomunozme <diego.munoz.me@gmail.com>
2022-07-14 07:10:54 -0700

    beginning FOA

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = props => {
+    return 
+    <React.Fragment>
+        <header>
+            <h1>React Meals</h1>
+            <button>Cart</button>
+        </header>
+        <div>
+
+        </div>
+    </React.Fragment>
+
+}
+
+export default Header;
