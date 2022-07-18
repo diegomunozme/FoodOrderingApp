0a3f26e9532ec173a23c7df81340b945c3c34aa9
diegomunozme <diego.munoz.me@gmail.com>
2022-07-18 09:27:07 -0600

    midcourse project progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <React.Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
-
-const Header = props => {
-    return 
+import mealsImg from "../../assets/meals.jpg";
+import classes from "./Header.module.css"
+import HeaderCartButton from "./HeaderCartButton"
+const Header = (props) => {
+  return (
     <React.Fragment>
-        <header>
-            <h1>React Meals</h1>
-            <button>Cart</button>
-        </header>
-        <div>
-
-        </div>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={ mealsImg } alt='meaningful text,'/>
+      </div>
     </React.Fragment>
-
-}
+  );
+};
 
 export default Header;
