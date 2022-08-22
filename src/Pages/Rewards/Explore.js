21838ae0440b50e02c9c57b2301ad5d5c94d6d5f
diegomunozme <diego.munoz.me@gmail.com>
2022-08-22 05:08:31 -0700

    added initial styling to product divs

--- src/Pages/Rewards/Explore.js
+++ src/Pages/Rewards/Explore.js
@@ -1,47 +1,23 @@
 import React from "react";
-import Card from "../../Components/UI/Modal/Card/Card";
 import classes from "./Explore.module.css";
-import UA from "../../assets/images/hbLogo.jpg";
 import { products } from "../../assets/data";
 
 const Explore = () => {
   return (
     <div>
       <div className={classes.exploreMainContent}>
-        <div className={classes.exploreTitle}>Hottest Deals</div>
-        <div className={classes.hottestDealsGrid}>
-          <Card>
-            <img src={UA} alt="placeholder" className={classes.exploreCard} />{" "}
-          </Card>
-          <Card>
-            <img src={UA} alt="placeholder" className={classes.exploreCard} />{" "}
-          </Card>
-          <Card>
-            <img src={UA} alt="placeholder" className={classes.exploreCard} />{" "}
-          </Card>
-          <Card>
-            <img src={UA} alt="placeholder" className={classes.exploreCard} />{" "}
-          </Card>
-        </div>
         <div>
           <div className={classes.exploreTitle}>Hottest Deals</div>
-          {/* LEts Try to import the new style */}
-          <div>
+          <div className={classes.productsBox}>
             <div className={classes.productsContainer}>
               {products.map((item) => (
                 <div className={classes.Container}>
-                  <div className={classes.Circle} />{" "}
+                  <div className={classes.Circle} />
                   <img src={item.img} key={item.id} className={classes.Image} />
+                  <div className={classes.productText}>{item.title}</div>
                 </div>
               ))}
             </div>
-
-            {/* {products.map((item) => (
-              <Card className={classes.allCompaniesImages}>
-                {" "}
-                <img src={item.img} key={item.id} />
-              </Card>
-            ))} */}
           </div>
         </div>
       </div>
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,26 +1,64 @@
 export const products = [
     {
         id:1, 
-        img: require("./images/Nike.jpg")
+        img: require("./images/Nike.jpg"),
+        title: 'Nike',
+        deal: 'New Addition'
     },
     {
         id:2, 
-        img: require("./images/UA.jpg")
+        img: require("./images/UA.jpg"),
+        title: 'Under Armour',
+        deal: 'New Addition'
     },
     {
         id:3, 
-        img: require("./images/Macy.jpg")
+        img: require("./images/Macy.jpg"),
+        title:'Macy',
+        deal: 'New Addition'
     },
     {
         id:4, 
-        img: require("./images/Walmart.jpg")
-    },
-    {
-        id:5, 
-        img: require("./images/UA.jpg")
+        img: require("./images/Walmart.jpg"),
+        title:'Walmart',
+        deal: 'New Addition'
     },
     {
         id:6, 
-        img: require("./images/Target.jpg")
+        img: require("./images/Target.jpg"),
+        title:'Target',
+        deal: 'New Addition',
+        
+    },
+    {
+        id:7, 
+        img: require("./images/Kohls.jpg"),
+        title:'Kohls',
+        deal: 'New Addition'
+    },
+    {
+        id:8, 
+        img: require("./images/sephora.jpg"),
+        title:'Sephora',
+        deal: 'New Addition'
+    },
+    {
+        id:9, 
+        img: require("./images/shein.jpg"),
+        title:'Shein',
+        deal: 'New Addition'
+
+    },
+    {
+        id:10, 
+        img: require("./images/ebay.jpg"),
+        title:'Ebay',
+        deal: 'New Addition'
+    },
+    {
+        id:11, 
+        img: require("./images/JCPenney.png"),
+        title: 'JCPenny',
+        deal: 'New Addition'
     }
 ]
